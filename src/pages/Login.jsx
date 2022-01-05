b6b30185dbf10b4b5219d093b08910bb1a775bc0
Erikh22l18 <e_2019100825C@uncp.edu.pe>
2022-01-05 14:53:34 -0500

    [UPDATED]

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,59 @@
-import React, { useRef } from 'react';
-import '@styles/Login.scss';
+import React, { useRef } from 'react'
+import '@styles/Login.scss'
 import logo from '@logos/logo_yard_sale.svg'
 
 const Login = () => {
-	const form= useRef(null)
+  const form = useRef(null)
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-		const formData = new FormData(form.current)
-		const data = {
-			email: formData.get('email'),
-			password: formData.get('password')
-		}
+    const formData = new FormData(form.current)
+    const data = {
+      email: formData.get('email'),
+      password: formData.get('password'),
+    }
 
-		console.log(data)
-	}
+    console.log(data)
+  }
 
-	return (
-		<div className="Login">
-			<div className="Login-container">
-				<img src={logo} alt="logo" className="logo" />
-				<form action="/" className="form" ref={ form }>
-					<label htmlFor="email" className="label">Email address</label>
-					<input type="text" name="email" placeholder="platzi@example.cm" className="input input-email" />
-					<label htmlFor="password" className="label">Password</label>
-					<input type="password" name="password" placeholder="*********" className="input input-password" />
-					<button type="submit" value="Log in" className="primary-button login-button"
-					onClick={handleSubmit}>Login</button>
-					<a href="/">Forgot my password</a>
-				</form>
-				<button className="secondary-button signup-button" >Sign up</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="Login">
+      <div className="Login-container">
+        <img src={logo} alt="logo" className="logo" />
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email" className="label">
+            Email address
+          </label>
+          <input
+            type="text"
+            name="email"
+            placeholder="platzi@example.cm"
+            className="input input-email"
+          />
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="*********"
+            className="input input-password"
+          />
+          <button
+            type="submit"
+            value="Log in"
+            className="primary-button login-button"
+            onClick={handleSubmit}
+          >
+            Login
+          </button>
+          <a href="/">Forgot my password</a>
+        </form>
+        <button className="secondary-button signup-button">Sign up</button>
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
